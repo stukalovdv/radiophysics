0120577eb77a84bf23bf181385d3c62426e552ce
Dmitry Stukalov <35774847+stukalovdv@users.noreply.github.com>
2022-02-20 13:11:44 +0300

    Update main.cpp

--- Working/main.cpp
+++ Working/main.cpp
@@ -67,7 +67,8 @@ class fdtd
 double simulation( double THETA_MULTIPLICATOR ,double NU_TILDA, double R2_TILDA, double DELTA ) // Функция вычислений
 {
     double KPD = 0.871;
-
+    double dr = NU_TILDA * R2_TILDA * DELTA;
+    double dt = dr / ( 2 * myCom.c );
 
     int NR = 15;
     //vector <int> MASSIV(SIZE);
