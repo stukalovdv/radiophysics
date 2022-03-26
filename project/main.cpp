0c7563fb8b9284a2129f7eca226e31cd778c175f
Dmitry Stukalov <35774847+stukalovdv@users.noreply.github.com>
2022-03-26 11:16:43 +0300

    Проверка

--- project/main.cpp
+++ project/main.cpp
@@ -24,7 +24,7 @@ double fdtd( double THETA, double NU_TILDA, double R2_TILDA, double DELTA )
     double V = c / sqrt( cos( THETA ) );                            //
     double MAIN_COEFFICIENT = c * dt / ( sin( THETA * THETA ) );    //
     double SUB_COEFFICIENT = cos( THETA );
-    if (THETA > M_PI / 2 - 0.05 || THETA < M_PI / 2 + 0.05)
+    if ( THETA > M_PI / 2 - 0.05 && THETA < M_PI / 2 + 0.05 )
     {
         SUB_COEFFICIENT = 0;
         MAIN_COEFFICIENT = c * dt;
