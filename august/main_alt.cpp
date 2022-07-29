b69c2925615d6ec4129130044a0aa9a15d11d169
Dmitry Stukalov <35774847+stukalovdv@users.noreply.github.com>
2022-07-29 10:05:17 +0300

    Create dimensionless and undemensionless equations files

--- august/main_alt.cpp
+++ august/main_alt.cpp
@@ -1,4 +1,4 @@
-//НЕобезразмененная задача
+// Обезразмененная задача
 #include <iostream>
 #include <vector>
 #include <fstream>
@@ -12,52 +12,53 @@ float c = 3e+10, OMEGA_P_0 = 3e+9, J0 = 1;                         // Глоба
 
 float fdtd( float THETA, float NU_TILDA, float R2_TILDA, float DELTA )
 {
-    float R1_tilda = R2_TILDA * ( 1 - DELTA );                     // Внутренний (обезразмеренный) радиус цилиндра
+    float R1_TILDA = R2_TILDA * ( 1 - DELTA );                     // Внутренний (обезразмеренный) радиус цилиндра
     float NU = NU_TILDA * OMEGA_P_0;                               // Частота соударений
-    float R1 = R1_tilda * c / OMEGA_P_0;                           // Внутренний радиус цилиндра
+    float R1 = R1_TILDA * c / OMEGA_P_0;                           // Внутренний радиус цилиндра
     float R2 = R2_TILDA * c / OMEGA_P_0;                           // Внешний радиус цилиндра
-    float dr = 0.01 * NU_TILDA * ( R2 - R1 );                      // Шаг по пространству
-    float dt = dr / ( c * 2 );                                     // Шаг по времени
-    float T_MAX = 2;                                               // Расчетное (обезразмеренное) время
-    int N_TIME = T_MAX / ( dt * OMEGA_P_0 );                       // Кол-во шагов по времени
-    //N_TIME = 25000;
+    //float dr = 0.01 * NU_TILDA * ( R2 - R1 );                      // Шаг по пространству
+    float dr = 0.01 * NU_TILDA * (R2_TILDA - R1_TILDA);
+    float dt = dr / (c * 2) * OMEGA_P_0;                                     // Шаг по времени
+    float T_MAX = 0.2;                                              // Расчетное (обезразмеренное) время
+    int N_TIME = T_MAX / dt;                       // Кол-во шагов по времени
 
     // Новые коэффициенты
-    float MAIN_COEFFICIENT = c * dt / ( sin( THETA ) * sin( THETA ) ); // ПЕРЕД dt НУЖНО СТАВИТЬ с, НО ТОГДА БУДЕТ СЛИШКОМ СИЛЬНОЕ ОТКЛОНЕНИЕ И ОШИБКА
-    float SUB_COEFFICIENT = cos( THETA );
-    //float SUB_COEFFICIENT = 1.37e-35;
+    float MAIN_COEFFICIENT = dt / ( sin( THETA ) * sin( THETA ) );
+    float SUB_COEFFICIENT = abs( cos( THETA ) );
+    SUB_COEFFICIENT = 0;
+    /*
     if ( THETA > M_PI / 2 - 0.005 && THETA < M_PI / 2 + 0.005 )
     {
         SUB_COEFFICIENT = 0;
         MAIN_COEFFICIENT = c * dt;
     }
+    */
 
     //N_TIME = 25000;
     vector <float> T( N_TIME );                                     // Вектор времени
     for ( int i = 0; i < N_TIME; i++ )                              //
     {                                                               //
         T[i] = dt * i;                                              //
-    }                                                               //
+    }
 
-
-    float R_MAX = R2 * 40;                                         // Расчетное пространство (по r)
+    float R_MAX = R2_TILDA * 40;                                          // Расчетное пространство (по r)
     int NR = ( R_MAX + dr ) / dr;                                   // Кол-во шагов по пространству
-    vector <float> r( NR ), r_tilda( NR ), r_alt( NR );            // Обозначаем r и 1/r
+    cout << "Nr=" << NR << "\tNt=" << N_TIME << endl;
+    vector <float> r( NR ), r_tilda( NR ), r_alt( NR );             // Обозначаем r и 1/r
     for ( int i = 0; i < NR; i++ )                                  //
     {                                                               //
         r[i] = dr + dr * i;                                         //
         r_alt[i] = 1 / r[i];                                        //
         r_tilda[i] = r[i] * OMEGA_P_0 / c;                          //
-    }                                                               //
+    }
 
-    int NR1 =  R1 / dr, NR2 = R2 / dr;                              // Начало и конец неоднородного слоя (в кол-ве узлов)
-
-    // Точка проверки значения поля от времени
-    int FIELD_CHECK_POINT = NR2, FIELD_CHECK_POINT_TILDA = FIELD_CHECK_POINT * dr * OMEGA_P_0 / c;
+    int NR1 =  R1_TILDA / dr, NR2 = R2_TILDA / dr;                  // Начало и конец неоднородного слоя (в кол-ве узлов)
+    cout << "Nr1=" << NR1 << "\tNr2=" << NR2 << endl;
+    int FIELD_CHECK_POINT = NR2;                                    // Точка проверки значения поля от времени
     if ( NR1 == 0 ) NR1 = 1;
 
 
-    vector <float> Fr( NR );                                       // Обозначаем F(r)
+    vector <float> Fr( NR );                                        // Обозначаем F(r)
     for ( int i = 0; i < NR1; i++ )
     {
         Fr[i] = 1;
@@ -72,18 +73,21 @@ float fdtd( float THETA, float NU_TILDA, float R2_TILDA, float DELTA )
     }
 
 
-    int N_PML = 10 * NR2;                                           // Поглощающий слой
-    vector <float> sigma( NR );                                    // Начало поглощения
-    for ( int i = 0; i < NR - N_PML; i++ )
+    int N_PML = 20 * NR2;                                           // Поглощающий слой
+    vector <float> sigma( NR );                                     // Начало поглощения
+    //for ( int i = 0; i < NR - N_PML; i++ )
+    for ( int i = 0; i < NR; i++ )
     {
         sigma[i] = 1;
     }
+    /*
     for ( int i = NR - N_PML; i < NR; i++ )
     {
-        sigma[i] =  cos( (float)( M_PI / 2 ) * ( i - ( NR - N_PML ) ) / ( NR - ( NR - N_PML ) ) );
+        sigma[i] = cos( (float)( M_PI / 2 ) * ( i - ( NR - N_PML ) ) / ( NR - ( NR - N_PML ) ) );
         //sigma[i] = 1;
     }
     sigma[NR - 1] = 0;
+    */
 
     vector <float> Er( NR ), Ephi( NR ), Ez( NR );                     // Проекции электрических полей
     vector <float> Hr( NR ), Hphi( NR ), Hz( NR );                     // Проекции магнитных полей
@@ -101,8 +105,8 @@ float fdtd( float THETA, float NU_TILDA, float R2_TILDA, float DELTA )
         Hphi[i] = 0;
         Hz[i] = 0;
 
-        Jr[i] = J0 * Fr[i];
-        Jphi[i] = - J0 * Fr[i];
+        Jr[i] = Fr[i];
+        Jphi[i] = - Fr[i];
         Jz[i] = 0;
     }
 
@@ -117,74 +121,66 @@ float fdtd( float THETA, float NU_TILDA, float R2_TILDA, float DELTA )
 
     for ( int n = 0; n < N_TIME; n++ )
     {
-        /*
         //Jr
         for ( int i = 0; i < NR; i++ )
         {
-            Jr[i] = J0 * sin( OMEGA_P_0 * n * dt );
+            Jr[i] = Jr[i] * ( 1 - dt * NU ) + dt * Fr[i] * Er[i];
         }
-        //Jphi
-        for ( int i = 0; i < NR; i++ )
-        {
-            Jphi[i] = - J0 * sin( OMEGA_P_0 * n * dt );
-        }*/
 
-        //Jr
-        for ( int i = 0; i < NR; i++ )
-        {
-            Jr[i] = Jr[i] * ( 1 - dt * NU ) + ( dt * OMEGA_P_0 * OMEGA_P_0 / ( 4 * M_PI ) ) * Fr[i] * Er[i];
-        }
         //Jphi
         for ( int i = 0; i < NR; i++ )
         {
-            Jphi[i] = Jphi[i] * ( 1 - dt * NU ) + ( dt * OMEGA_P_0 * OMEGA_P_0 / ( 4 * M_PI ) ) * Fr[i] * Ephi[i];
+            Jphi[i] = Jphi[i] * ( 1 - dt * NU ) + dt * Fr[i] * Ephi[i];
         }
+
         //Jz
         for ( int i = 0; i < NR; i++ )
         {
-            Jz[i] = Jz[i] * ( 1 - dt * NU ) + ( dt * OMEGA_P_0 * OMEGA_P_0 / ( 4 * M_PI ) ) * Fr[i] * Ez[i];
+            Jz[i] = Jz[i] * ( 1 - dt * NU ) + dt * Fr[i] * Ez[i];
         }
 
         //Er
         Er[0] = Er[1];
         for ( int i = 1; i < NR; i++ )
         {
-            Er[i] = sigma[i] * ( Er[i] + MAIN_COEFFICIENT * ( r_alt[i] * Hz[i] - ( 4 * M_PI / c ) * Jr[i] + SUB_COEFFICIENT * ( Ez[i] - Ez[i - 1] ) / dr ) );
+            Er[i] = sigma[i] * ( Er[i] + MAIN_COEFFICIENT * ( r_alt[i] * Hz[i] - Jr[i] + SUB_COEFFICIENT * ( Ez[i] - Ez[i - 1] ) / dr ) );
         }
+
         //Ephi
         Ephi[0] = Ephi[1];
         for ( int i = 1; i < NR; i++ )
         {
-            Ephi[i] = sigma[i] * ( Ephi[i] - ( c * dt / dr ) * ( Hz[i] - Hz[i-1] ) - ( 4 * M_PI * dt ) * Jphi[i] );
+            Ephi[i] = sigma[i] * ( Ephi[i] - MAIN_COEFFICIENT * ( SUB_COEFFICIENT * r_alt[ i ] * Ez[ i ] + ( 1 / dr ) * ( Hz[i] - Hz[ i - 1 ] ) + Jphi[i] ) );
         }
 
         //Ez
         Ez[0] = Ez[1];
         for ( int i = 1; i < NR; i++ )
         {
-            Ez[i] = sigma[i] * ( Ez[i] + ( c * dt * r_alt[i] ) * ( ( Hphi[i] * r[i] - Hphi[i - 1] * r[i - 1] ) / dr - Hr[i] ) - 4 * M_PI * dt * Jz[i] );
+            Ez[i] = sigma[i] * ( Ez[i] + ( dt * r_alt[i] ) * ( ( Hphi[i] * r[i] - Hphi[i - 1] * r[i - 1] ) / dr - Hr[i] ) - dt * Jz[i] );
         }
-        Ez[NR - 1] = sigma[NR - 1] * ( Ez[NR - 1] - ( c * dt * r_alt[NR - 1] ) * Hr[NR - 1] );
-
 
         //Hr
         Hr[0] = Hr[1];
         for ( int i = 1; i < NR; i++ )
         {
-            Hr[i] = sigma[i] * ( Hr[i] + MAIN_COEFFICIENT * ( r_alt[i] * Ez[i] + SUB_COEFFICIENT * ( ( Hz[i] - Hz[i - 1] ) / dr + ( 4 * M_PI / c ) * Jphi[i] ) ) );
+            Hr[i] = sigma[i] * ( Hr[i] + MAIN_COEFFICIENT * ( r_alt[i] * Ez[i] + SUB_COEFFICIENT * ( ( Hz[i] - Hz[i - 1] ) / dr + Jphi[i] ) ) );
         }
+
         //Hphi
         Hphi[0] = Hphi[1];
         for ( int i = 1; i < NR; i++ )
         {
-            Hphi[i] = sigma[i] * ( Hphi[i] + MAIN_COEFFICIENT * ( SUB_COEFFICIENT * ( r_alt[i] * Hz[i] - ( 4 * M_PI / c ) * Jr[i] ) + ( Ez[i] - Ez[i - 1] ) / dr ) );
+            Hphi[i] = sigma[i] * ( Hphi[i] + MAIN_COEFFICIENT * ( SUB_COEFFICIENT * ( r_alt[i] * Hz[i] - Jr[i] ) + ( Ez[i] - Ez[i - 1] ) / dr ) );
         }
+
         //Hz
-        for ( int i = 0; i < NR - 1; i++ )
+        Hz[0] = Hz [1];
+        for ( int i = 1; i < NR; i++ )
         {
-            Hz[i] = sigma[i] * ( Hz[i] - ( c * dt * r_alt[i] ) * ( Er[i] + ( r[i + 1] * Ephi[i + 1] - r[i] * Ephi[i] ) / dr ) );
+            Hz[i] = sigma[i] * ( Hz[i] - dt * r_alt[i] * ( Er[i] + ( r[i + 1] * Ephi[i + 1] - r[i] * Ephi[i] ) / dr ) );
         }
-        Hz[ NR - 1 ] = sigma[ NR - 1 ] * ( Hz[ NR - 1 ] - ( c * dt * r_alt[ NR - 1 ] ) * Er[ NR - 1 ]);
+
 
 
         Ept[n] = Ephi[FIELD_CHECK_POINT];
@@ -193,8 +189,9 @@ float fdtd( float THETA, float NU_TILDA, float R2_TILDA, float DELTA )
         Ezt[n] = Ez[FIELD_CHECK_POINT];
         Hpt[n] = Hphi[FIELD_CHECK_POINT];
 
+
         cout << "Loading... " << ( n * 100 / N_TIME ) + 1 << "/" << 100 << "%\r";
-        fout << left << setw( 11 ) << T[n] * OMEGA_P_0 << "\t";
+        fout << left << setw( 11 ) << T[n] << "\t";
         fout << left << setw( 11 ) << Er[FIELD_CHECK_POINT] << "\t" << left << setw( 11 ) << Ephi[FIELD_CHECK_POINT] << "\t" << left << setw( 11 ) << Ez[FIELD_CHECK_POINT] << "\t";
         fout << left << setw( 11 ) << Hr[FIELD_CHECK_POINT] << "\t" << left << setw( 11 ) << Hphi[FIELD_CHECK_POINT] << "\t" << left << setw( 11 ) << Hz[FIELD_CHECK_POINT] << "\t";
         fout << left << setw( 11 ) << Jr[( NR2 + NR1 ) / 2] << "\t" << left << setw( 11 ) << Jphi[( NR2 + NR1 ) / 2] << "\t" << left << setw( 11 ) << Jz[( NR2 + NR1 ) / 2] << "\t";
@@ -247,7 +244,7 @@ int main()
     }
     while ( DELTA < 0 || DELTA > 1 );
 
-    cout << fdtd( THETA_MULTIPLICATOR * M_PI, NU_TILDA, R2_TILDA, DELTA );
+    cout << fdtd( THETA_MULTIPLICATOR * (long double)M_PI, NU_TILDA, R2_TILDA, DELTA );
     cout << "\a" <<endl;
 
     //cout << THETA_MULTIPLICATOR * M_PI << "  " << cos( THETA_MULTIPLICATOR * M_PI );
@@ -260,9 +257,5 @@ int main()
     //if (Fr.size() == r.size( ) ) cout << "\nYES";
 
     //cout << r.size() << "\t" << Fr.size() << "\t" << Nr << endl;
-
-
-
-
     return 0;
 }
